e1220f3f0c7d93adec4ef119737e2d7b27fa2101
Fabio Niephaus <code@fniephaus.com>
2026-07-05 18:00:18 +0200

    Build with `-march=x86-64-v3`

--- mx.trufflesqueak/mx_trufflesqueak.py
+++ mx.trufflesqueak/mx_trufflesqueak.py
@@ -32,7 +32,7 @@ def trufflesqueak_standalone_deps():
 def libsmalltalkvm_build_args():
     build_args = []
     selected_march = (
-        "x86-64-v2"
+        "x86-64-v3"
         if mx.get_arch() == "amd64"
         else ("armv8.1-a" if mx.get_arch() == "aarch64" else "compatibility")
     )
