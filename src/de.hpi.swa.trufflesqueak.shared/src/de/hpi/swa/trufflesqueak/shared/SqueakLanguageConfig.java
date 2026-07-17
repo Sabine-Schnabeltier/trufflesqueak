c9cb13e601ec13dd1d0e3a81dfca89a0952e32df
Sabine Schnabeltier <34526618+Sabine-Schnabeltier@users.noreply.github.com>
2026-07-17 02:55:15 -0500

    Use context stack depth to set the Java stack size (#290)

--- src/de.hpi.swa.trufflesqueak.shared/src/de/hpi/swa/trufflesqueak/shared/SqueakLanguageConfig.java
+++ src/de.hpi.swa.trufflesqueak.shared/src/de/hpi/swa/trufflesqueak/shared/SqueakLanguageConfig.java
@@ -19,6 +19,8 @@ public final class SqueakLanguageConfig {
     public static final String WEBSITE = "https://github.com/hpi-swa/trufflesqueak";
     private static final String IMAGE_VERSION = "25.0.1"; // on release: `VERSION;`
 
+    public static final int DEFAULT_CONTEXT_STACK_DEPTH = 2 << 12;
+
     public record SupportedImage(String id, String name, String url) {
         public static SupportedImage url(final String url) {
             final String name = extractName(url);
