7edd5ee02e3fb0e938a66933d53a50a296c48080
Fabio Niephaus <code@fniephaus.com>
2026-07-22 19:28:43 +0200

    Various cleanups and fixes

--- src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/dispatch/DispatchSelectorNaryNode.java
+++ src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/dispatch/DispatchSelectorNaryNode.java
@@ -313,7 +313,7 @@ public final class DispatchSelectorNaryNode extends AbstractDispatchSelectorNode
                 if (primitiveNode != null) {
                     return new DispatchDirectPrimitiveNaryNode(assumptions, method, primitiveNode);
                 }
-                DispatchUtils.logMissingPrimitive(primitiveNode, method);
+                DispatchUtils.logMissingPrimitive(null, method);
             }
             return new DispatchDirectMethodNaryNode(assumptions, method);
         }
