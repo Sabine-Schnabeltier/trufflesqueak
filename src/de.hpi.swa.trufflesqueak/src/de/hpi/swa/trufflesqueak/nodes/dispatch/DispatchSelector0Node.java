4d114ebc14d126370d8d97bbfaeba7472b5050e3
Sabine Schnabeltier <sabine.schnabeltier@gmail.com>
2026-07-31 15:19:54 -0500

    add guards to wide nodes; merge fast and wide nodes to preserve profiling data

--- src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/dispatch/DispatchSelector0Node.java
+++ src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/dispatch/DispatchSelector0Node.java
@@ -66,7 +66,7 @@ public final class DispatchSelector0Node extends AbstractDispatchSelectorNode {
         @ExplodeLoop
         @InliningCutoff
         public Object execute(final VirtualFrame frame, final Object receiver) {
-            // TIER 4: Megamorphic Fallback (Indirect Execution)
+            // TIER 3: Megamorphic Fallback (Indirect Execution)
             if (indirectNode != null) {
                 return indirectNode.execute(frame, true, selector, receiver);
             }
@@ -77,21 +77,21 @@ public final class DispatchSelector0Node extends AbstractDispatchSelectorNode {
             }
 
             // TIER 1: Direct Execution Fast Path
-            for (final FastDispatchDataNode<DispatchDirect0Node> currentFast : cache.fastNodes) {
-                if (currentFast.guardChainNode.execute(receiver)) {
-                    return currentFast.dispatchDirectNode.execute(frame, receiver);
+            for (final DispatchEntry<DispatchDirect0Node> entry : cache.fastEntries) {
+                if (entry.isFastCacheHit(receiver)) {
+                    return entry.executor.execute(frame, receiver);
                 }
             }
 
             // TIER 2: Wide Execution (Class Polymorphism)
-            if (cache.wideNodes.length > 0) {
+            if (cache.wideEntries.length > 0) {
                 final ClassObject receiverClass = cache.classNode.executeLookup(cache, receiver);
                 final Object lookupResult = getContext().lookup(receiverClass, selector);
 
                 if (lookupResult instanceof CompiledCodeObject targetMethod) {
-                    for (final WideDispatchDataNode<DispatchDirect0Node> currentWide : cache.wideNodes) {
-                        if (currentWide.standardMethod == targetMethod) {
-                            return currentWide.dispatchDirectNode.execute(frame, receiver);
+                    for (final DispatchEntry<DispatchDirect0Node> entry : cache.wideEntries) {
+                        if (entry.isWideCacheHit(targetMethod)) {
+                            return entry.executor.execute(frame, receiver);
                         }
                     }
                 }
--- src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/dispatch/DispatchSelector1Node.java
+++ src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/dispatch/DispatchSelector1Node.java
@@ -65,7 +65,7 @@ public final class DispatchSelector1Node extends AbstractDispatchSelectorNode {
         @ExplodeLoop
         @InliningCutoff
         public Object execute(final VirtualFrame frame, final Object receiver, final Object arg1) {
-            // TIER 4: Megamorphic Fallback (Indirect Execution)
+            // TIER 3: Megamorphic Fallback (Indirect Execution)
             if (indirectNode != null) {
                 return indirectNode.execute(frame, true, selector, receiver, arg1);
             }
@@ -76,21 +76,21 @@ public final class DispatchSelector1Node extends AbstractDispatchSelectorNode {
             }
 
             // TIER 1: Direct Execution Fast Path
-            for (final FastDispatchDataNode<DispatchDirect1Node> currentFast : cache.fastNodes) {
-                if (currentFast.guardChainNode.execute(receiver)) {
-                    return currentFast.dispatchDirectNode.execute(frame, receiver, arg1);
+            for (final DispatchEntry<DispatchDirect1Node> entry : cache.fastEntries) {
+                if (entry.isFastCacheHit(receiver)) {
+                    return entry.executor.execute(frame, receiver, arg1);
                 }
             }
 
             // TIER 2: Wide Execution (Class Polymorphism)
-            if (cache.wideNodes.length > 0) {
+            if (cache.wideEntries.length > 0) {
                 final ClassObject receiverClass = cache.classNode.executeLookup(cache, receiver);
                 final Object lookupResult = getContext().lookup(receiverClass, selector);
 
                 if (lookupResult instanceof CompiledCodeObject targetMethod) {
-                    for (final WideDispatchDataNode<DispatchDirect1Node> currentWide : cache.wideNodes) {
-                        if (currentWide.standardMethod == targetMethod) {
-                            return currentWide.dispatchDirectNode.execute(frame, receiver, arg1);
+                    for (final DispatchEntry<DispatchDirect1Node> entry : cache.wideEntries) {
+                        if (entry.isWideCacheHit(targetMethod)) {
+                            return entry.executor.execute(frame, receiver, arg1);
                         }
                     }
                 }
